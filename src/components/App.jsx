ef34dfb5adcbca908cc9b57b10c03b45c1ed41f6
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-24 21:50:00 +0200

    code refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,87 +1,73 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
 import { FeedbackOptions } from './feedbackoptions/FeedbackOptions';
 import { Section } from './section/Section';
 import { Statistics } from './statistics/Statistics';
 import { Notification } from 'components/notification/Notification';
 const options = ['Good', 'Neutral', 'Bad'];
 
-export class Feedback extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-      total: 0,
-      positiveFeedback: 0,
-    };
-  }
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [positiveFeedback, setPositiveFeedback] = useState(0);
 
-  countPositiveFeedback = () => {
-    if (this.state.total === 0) {
-      return 0;
+  const countPositiveFeedback = () => {
+    if (good === 0) {
+      setPositiveFeedback(0);
     } else {
-      this.setState({
-        positiveFeedback: Math.round(
-          (this.state.good / this.state.total) * 100
-        ),
-      });
+      setPositiveFeedback(Math.round((good / total) * 100));
     }
   };
-  countTotalFeedback = () => {
-    this.setState({ total: this.state.total + 1 });
+  const countTotalFeedback = () => {
+    setTotal(total + 1);
   };
 
-  handleAddGoodReview = () => {
+  const handleAddGoodReview = () => {
     console.log('Good was clicked');
-    this.setState({ good: this.state.good + 1 });
-    this.countTotalFeedback();
-    this.countPositiveFeedback();
+    setGood(good + 1);
+    countTotalFeedback();
+    countPositiveFeedback();
   };
-  handleAddNeutralReview = () => {
+  const handleAddNeutralReview = () => {
     console.log('Neutral was clicked');
-    this.setState({
-      neutral: this.state.neutral + 1,
-    });
-    this.countTotalFeedback();
-    this.countPositiveFeedback();
+    setNeutral(neutral + 1);
+    countTotalFeedback();
+    countPositiveFeedback();
   };
-  handleAddBadReview = () => {
+  const handleAddBadReview = () => {
     console.log('Bad was clicked');
-    this.setState({ bad: this.state.bad + 1 });
-    this.countTotalFeedback();
-    this.countPositiveFeedback();
+    setBad(bad + 1);
+    countTotalFeedback();
+    countPositiveFeedback();
   };
 
-  render() {
-    return (
-      <div>
-        <Section variant="section" title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={[
-              this.handleAddGoodReview,
-              this.handleAddNeutralReview,
-              this.handleAddBadReview,
-            ]}
+  return (
+    <div>
+      <Section variant="section" title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={[
+            handleAddGoodReview,
+            handleAddNeutralReview,
+            handleAddBadReview,
+          ]}
+        />
+        <h2>Statistics</h2>
+        {total === 0 ? (
+          <Notification variant="notification">
+            There is no feedback.
+          </Notification>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positiveFeedback}
           />
-          <h2>Statistics</h2>
-          {this.state.total === 0 ? (
-            <Notification variant="notification">
-              There is no feedback.
-            </Notification>
-          ) : (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.state.total}
-              positivePercentage={this.state.positiveFeedback}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        )}
+      </Section>
+    </div>
+  );
+};
